a01f4a781d119bcf80ff287a521f68597fd1ce11
Kazuki Sawada <kazuki@6715.jp>
2021-04-29 15:39:35 +0900

    split docker-compose.yml

--- compose/compose.sh
+++ compose/compose.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+IMAGE="docker/compose:alpine-1.29.1"
+
 docker run --rm \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v $PWD:$PWD \
 	--workdir $PWD \
-	docker/compose "$@"
+	$IMAGE "$@"
