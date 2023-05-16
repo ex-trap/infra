54245b7a67305fab1d3c5bbe887ed0e4465aeb43
Kazuki Sawada <kazuki@6715.jp>
2023-05-17 01:46:05 +0900

    freeze container version

--- compose/compose.sh
+++ compose/compose.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-IMAGE="docker/compose:alpine-1.29.2"
-
 docker run --rm \
-	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v $PWD:$PWD \
+	--volume /var/run/docker.sock:/var/run/docker.sock \
+	--volume $PWD:$PWD \
 	--workdir $PWD \
-	$IMAGE "$@"
+	docker compose "$@"
